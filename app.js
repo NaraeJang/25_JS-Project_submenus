ab4d7b9f02aed4948f4ccd75582fc66d98b308c5
NaraeJang <j.naaaarae@gmail.com>
2023-08-01 23:15:38 -0400

    Sidebar Toggle

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 import sublinks from "./data.js";
 
-console.log(sublinks);
+const get = (element) => {
+  const selection = document.querySelector(element);
+
+  if (selection) return selection;
+
+  throw new Error("No element selected");
+};
+
+const toggleBtn = get(".toggle-btn"),
+  closeBtn = get(".close-btn"),
+  sidebarWrapper = get(".sidebar-wrapper"),
+  sidebar = get(".sidebar-links"),
+  linkBtn = [...document.querySelectorAll(".link-btn")],
+  submenu = get(".submenu"),
+  hero = get(".hero"),
+  nav = get(".nav");
+
+// hide.show sidebar
+toggleBtn.addEventListener("click", () => {
+  sidebarWrapper.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebarWrapper.classList.remove("show");
+});
